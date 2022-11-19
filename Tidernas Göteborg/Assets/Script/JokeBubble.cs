5a2fa9d44320772fc843442c731ad116577c7e77
Carl Christensen <carl.christensen98@gmail.com>
2022-11-19 18:35:37 +0100

    Added JokeManager

--- Tidernas Göteborg/Assets/Script/JokeBubble.cs
+++ Tidernas Göteborg/Assets/Script/JokeBubble.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class JokeBubble : MonoBehaviour
 {
@@ -9,17 +11,76 @@ public class JokeBubble : MonoBehaviour
 
     private List<GameObject> jokes, punchLines;
     private List<int> usedJokes;
-    void Start()
+
+    void Awake()
     {
         usedJokes = new List<int>();
-        jokes = Resources.LoadAll<GameObject>("/Jokes/").ToList();
-        punchLines = Resources.LoadAll<GameObject>("/PunchLines/").ToList();
+        jokes = Resources.LoadAll<GameObject>("Jokes/").ToList();
+        punchLines = Resources.LoadAll<GameObject>("PunchLines/").ToList();
     }
 
-    private void TellJoke()
+    private void OnEnable()
     {
-        float randomJoke = Random.Range(0, 11);
+        MoneyCounter.tjuckeGrow += TellJoke;
+    }
 
+    private async void TellJoke()
+    {
+        if(usedJokes.Count == 10)
+        {
+            usedJokes.Clear();
+        }
 
+        int randomJoke = Random.Range(0, 10);
+        if (usedJokes.Contains(randomJoke))
+        {
+            TellJoke();
+            return;
+        }
+        usedJokes.Add(randomJoke);
+        await AwaitJoke(randomJoke);
+        await GracePeriod();
+        await AwaitPunchLine(randomJoke);
+    }
+
+    private async Task AwaitJoke(int index)
+    {
+        float endTime = Time.time + 3;
+        speechBubble.SetActive(true);
+        GameObject text = Instantiate(jokes[index], speechBubble.transform);
+        while(Time.time < endTime)
+        {
+            await Task.Yield();
+        }
+        Destroy(text);
+        speechBubble.SetActive(false);
+    }
+
+    private async Task GracePeriod()
+    {
+        float endTime = Time.time + 1;
+
+        while (Time.time < endTime)
+        {
+            await Task.Yield();
+        }
+    }
+
+    private async Task AwaitPunchLine(int index)
+    {
+        float endTime = Time.time + 3;
+        speechBubble.SetActive(true);
+        GameObject text = Instantiate(punchLines[index], speechBubble.transform);
+        while (Time.time < endTime)
+        {
+            await Task.Yield();
+        }
+        Destroy(text);
+        speechBubble.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        MoneyCounter.tjuckeGrow -= TellJoke;
     }
 }
