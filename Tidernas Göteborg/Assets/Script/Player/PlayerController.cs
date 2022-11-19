13ac2c4034595599fad8902446d19a35676a3252
Carl Christensen <carl.christensen98@gmail.com>
2022-11-19 11:55:08 +0100

    Update PlayerController.cs

--- Tidernas Göteborg/Assets/Script/Player/PlayerController.cs
+++ Tidernas Göteborg/Assets/Script/Player/PlayerController.cs
@@ -24,11 +24,10 @@ public class PlayerController : MonoBehaviour
     {
         movement = new Vector3(horizontal.ReadValue<float>(), 0, vertical.ReadValue<float>()).normalized;
         rigidBody.velocity += movement;
-        if(movement.magnitude > 0)
+        if(rigidBody.velocity.magnitude > 10)
             clampedVelocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -10, 10), 0, Mathf.Clamp(rigidBody.velocity.z, -10, 10)).normalized * 10;
         else
             clampedVelocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -10, 10), 0, Mathf.Clamp(rigidBody.velocity.z, -10, 10));
         rigidBody.velocity = clampedVelocity;
-        print(rigidBody.velocity.magnitude);
     }
 }
