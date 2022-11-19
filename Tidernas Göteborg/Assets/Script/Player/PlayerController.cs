e26e97cf711c3fa105f445524f52c35c0aa181ee
Carl Christensen <carl.christensen98@gmail.com>
2022-11-19 13:36:05 +0100

    Added slip method

--- Tidernas Göteborg/Assets/Script/Player/PlayerController.cs
+++ Tidernas Göteborg/Assets/Script/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +11,7 @@ public class PlayerController : MonoBehaviour
     InputAction vertical, horizontal;
     Vector3 movement, clampedVelocity;
     float speed = 10;
+    private bool canMove = true;
 
     void Start()
     {
@@ -17,20 +19,41 @@ public class PlayerController : MonoBehaviour
         inputController.Enable();
         vertical = inputController.Movement.Vertical;
         horizontal = inputController.Movement.Horizontal;
+        inputController.Actions.Debug.performed += SlipOnFish;
         rigidBody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        movement = new Vector3(horizontal.ReadValue<float>(), 0, vertical.ReadValue<float>()).normalized;
-        rigidBody.velocity += movement;
-        if(rigidBody.velocity.magnitude > 10)
-            clampedVelocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -10, 10), 0, Mathf.Clamp(rigidBody.velocity.z, -10, 10)).normalized * speed;
-        else
-            clampedVelocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -10, 10), 0, Mathf.Clamp(rigidBody.velocity.z, -10, 10));
-        rigidBody.velocity = clampedVelocity;
+        if (canMove)
+        {
+            movement = new Vector3(horizontal.ReadValue<float>(), 0, vertical.ReadValue<float>()).normalized;
+            rigidBody.velocity += movement;
+            if(rigidBody.velocity.magnitude > 10)
+                clampedVelocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -10, 10), 0, Mathf.Clamp(rigidBody.velocity.z, -10, 10)).normalized * speed;
+            else
+                clampedVelocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -10, 10), 0, Mathf.Clamp(rigidBody.velocity.z, -10, 10));
+            rigidBody.velocity = clampedVelocity;
 
+            transform.rotation = rigidBody.velocity == Vector3.zero ? transform.rotation : Quaternion.LookRotation(rigidBody.velocity);
+        }
+    }
 
-        transform.rotation = rigidBody.velocity == Vector3.zero ? transform.rotation : Quaternion.LookRotation(rigidBody.velocity);
+    public async void SlipOnFish(InputAction.CallbackContext ctx)
+    {
+        Vector3 velocity = rigidBody.velocity;
+        await DeactivateControls(0.2f, velocity);
+    }
+
+    public async Task DeactivateControls(float time, Vector3 velocity)
+    {
+        canMove = false;
+        float endTime = Time.time + time;
+        while(Time.time < endTime)
+        {
+            rigidBody.velocity = velocity * 2f;
+            await Task.Yield();
+        }
+        canMove = true;
     }
 }
