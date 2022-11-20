6066c576f3f8e85ba67627dbef77ffc7ea6111d9
Carl Christensen <carl.christensen98@gmail.com>
2022-11-20 12:21:42 +0100

    Update PauseManager.cs

--- Tidernas Göteborg/Assets/Script/UI/PauseManager.cs
+++ Tidernas Göteborg/Assets/Script/UI/PauseManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PauseManager : MonoBehaviour
 {
     public GameObject pauseScreen;
+    public GameObject HUD;
     private InputController inputController;
 
     void Start()
@@ -20,6 +21,7 @@ public class PauseManager : MonoBehaviour
         if(Time.timeScale > 0) Time.timeScale = 0;
         else Time.timeScale = 1;
 
+        HUD.SetActive(!HUD.activeSelf);
         pauseScreen.SetActive(!pauseScreen.activeSelf);
     }
 }
