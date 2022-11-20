1415d94af626bf172b710dc79d3c68303ef88760
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-11-20 17:46:15 +0100

    last commit

--- Tidernas Göteborg/Assets/DontDestroy.cs
+++ Tidernas Göteborg/Assets/DontDestroy.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    private static DontDestroy _instance;
+    public static DontDestroy INSTANCE { get { return _instance; } }
+
     private void Awake()
     {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+            Destroy(gameObject);
+
         DontDestroyOnLoad(this);
     }
 }
