739f2324be891219c0494527f8cc37400162e698
NeoLjusica <jessiicaceder@hotmail.com>
2022-11-20 16:09:00 +0100

    Tjocketjock

--- Tidernas Göteborg/Assets/Script/TjuckeFlyEndingScene.cs
+++ Tidernas Göteborg/Assets/Script/TjuckeFlyEndingScene.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class TjuckeFlyEndingScene : MonoBehaviour
 {
+    public GameObject endMenu;
+
     private Rigidbody rigidBody;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         rigidBody.AddForce(10, 20, 10);
+
+        StartCoroutine(nameof(WaitForEnding));
     }
+
+    IEnumerator WaitForEnding()
+    {
+        yield return new WaitForSeconds(7);
+        endMenu.gameObject.SetActive(true);
+    }
+
 }
